5ddbe72df8e19d846267e3d43768996de195b797
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2025-03-19 21:33:43 +0100

    optionally colored output

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
 import { colorPrint } from "./colored_c.ts";
 import { gen, repr } from "./mod.ts";
 
+function pprint(value: string = "", useColor = false) {
+  if (useColor) {
+    colorPrint(value);
+  } else {
+    console.log(value);
+  }
+}
+
 if (import.meta.main) {
+  const useColor = Deno.args.includes("--color");
+
   const def: repr.hir.Struct[] = [
     {
       name: "receipts_one_res",
@@ -16,11 +26,12 @@ if (import.meta.main) {
     .map(repr.mir.fromHir)
     .map(repr.fromMir);
 
-  colorPrint(
+  pprint(
     tree.map(gen.c.typedef.structDef).join("\n\n"),
+    useColor,
   );
-  console.log();
-  colorPrint(tree.map(gen.c.json.deserializerDef).join("\n\n"));
-  console.log();
-  colorPrint(tree.map(gen.c.json.deserializerImpl).join("\n\n"));
+  pprint();
+  pprint(tree.map(gen.c.json.deserializerDef).join("\n\n"), useColor);
+  pprint();
+  pprint(tree.map(gen.c.json.deserializerImpl).join("\n\n"), useColor);
 }
