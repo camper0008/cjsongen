e5771aba8ba0b28215b23f98bc076ca8a1a1e9c9
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2025-03-31 22:27:42 +0200

    sqlite setup create array/struct

--- gen/sql/sqlite/mod.ts
+++ gen/sql/sqlite/mod.ts
@@ -1,16 +1,28 @@
-import { Node, StructNode } from "../../../repr/node.ts";
+import { ArrayNode, Node, StructNode } from "../../../repr/node.ts";
 import { NodeMap } from "../../nodemap.ts";
 import { Output } from "../../output.ts";
 
+function createArray(node: ArrayNode, map: NodeMap): Output {
+    const out = new Output();
+    out.push(node.key);
+    return out;
+}
+
 function createStruct(node: StructNode, map: NodeMap): Output {
-    return new Output();
+    const out = new Output();
+    out.push(node.key);
+    return out;
 }
 
 export function create(node: Node[]): string {
     const map = new NodeMap(node);
     return node
-        .filter((node) => node.tag === "struct")
-        .map((node) => createStruct(node, map))
+        .filter((node) => node.tag === "struct" || node.tag === "array")
+        .map((node) =>
+            node.tag === "struct"
+                ? createStruct(node, map)
+                : createArray(node, map)
+        )
         .map((node) => node.eval())
         .join("\n");
 }
